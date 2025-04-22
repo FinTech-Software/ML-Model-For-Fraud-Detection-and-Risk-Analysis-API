2bc7afc75448c1d0b19508209967981c2c6b5282
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2025-04-23 00:37:47 +0530

    Update Frontent

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app &
 
 # Run Streamlit (on port 8080 or $PORT)
-streamlit run streamlit_app.py --server.port 8080 --server.address 0.0.0.0
+streamlit run app/frontend/streamlit_app.py --server.port 8080 --server.address 0.0.0.0
