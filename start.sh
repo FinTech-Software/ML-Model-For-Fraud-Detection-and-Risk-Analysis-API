ed6f7d22b2098f2fcf27592e551b33b64cb7328a
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2025-04-23 00:50:07 +0530

    Updated start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app &
 
 # Run Streamlit frontend on the exposed $PORT
-streamlit run streamlit_app.py --server.port $PORT --server.address 0.0.0.0
+streamlit run app/frontend/streamlit_app.py --server.port $PORT --server.address 0.0.0.0
